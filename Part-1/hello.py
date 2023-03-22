3cc4ed5bc8c894290bc9412f6038b402f148934b
Ajay Kumar <102378038+ajaykumar2pp@users.noreply.github.com>
2023-03-22 13:39:39 +0530

    firt program run

---
+++ Part-1/hello.py
@@ -0,0 +1,15 @@
+print("Ajay Kumar")
+
+# Some extension Install for python 
+# Coder Runner
+# autopep8
+# Pylint 
+
+# *********************  Single Line Comment *******************//
+
+print("Vijay kumar ")
+
+# Multi Line Comment  ***************
+'''print("Raja Kumar")
+print("sagar kumar")
+print("Shivam Kumar") '''
